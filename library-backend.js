c11ba2af1fcb6ca766dc792498c54f791d33f38c
Marco Richetta <marcorichetta@gmail.com>
2019-12-20 19:18:55 -0300

    Corrected some typos

--- library-backend.js
+++ library-backend.js
@@ -27,9 +27,9 @@ let authors = [
 ]
 
 /*
- * It would be more sensible to assosiate book and the author by saving 
+ * It would be more sensible to associate book and the author by saving 
  * the author id instead of the name to the book.
- * For simplicity we however save the author name.
+ * For simplicity we, however, save the author name.
 */
 
 let books = [
